15d9c7336207d6eb62d8381d8156c1f71663c0dd
Alexander Wilson van Deurs <alexwvd@outlook.com>
2017-07-20 12:57:11 +0200

    Login Files.

---
+++ resources/js/main.js
@@ -0,0 +1,36 @@
+var count = 5;
+
+/* Validation function that check if the user is in the (temporary)database*/
+function validate() {
+    var un = document.login.username.value; /* Username Input variable*/
+    var pw = document.login.password.value; /* Password Input variable*/
+    var valid = false;
+    
+    let data = responseData;
+    for(var account in data.authenticatUser){
+        if( un == responseData.authenticatUser.RSP.USR.CONT.EML){
+            valid = true;
+            break;
+        }
+    }
+    
+    
+    
+    if (valid) {
+        alert("Login was successful. Welcome, " + responseData.authenticatUser.RSP.USR.CONT.FNM + ".")
+        window.location = "https://www.google.com";
+        return false;
+    }
+    
+    if (count >= 1) {
+        alert("Incorrect Username or Password.");
+        count--;
+    }
+    else {
+        alert("You reached the maximum amount of login attempts. You are now blocked.");
+        document.login.username.value = "Blocked";
+        document.login.password.value = "Blocked";
+        document.login.username.disabled = true;
+        document.login.password.disabled = true;
+    }
+}
---
+++ resources/js/temporaryData.js
@@ -0,0 +1,257 @@
+var responseData = {
+  authenticatUser: {  
+   "ERR":0,
+   "RSP":{  
+      "AUTHC":"true",
+      "USR":{  
+         "ID":"2",
+         "TJT":"FULL",
+         "ACTV":"true",
+         "BO":"1489760664786",
+         "CONT":{  
+            "FNM":"John",
+            "LNM":"Doe",
+            "PHN":"5556667777",
+            "PHNTP":"NONE",
+            "EML":"ex@mple.com",
+            "EMLTP":"NONE"
+         },
+         "ADMIN":"false",
+         "LLOGN":"1489760664786",
+         "ACCT":{  
+            "ID":"2",
+            "TJT":"ID"
+         }
+      }
+   }
+},
+    getUserAccountDetails: {  
+   "ERR":0,
+   "RSP":{  
+      "ACCT":{  
+         "ID":"2",
+         "TJT":"FULL",
+         "ACTV":"true",
+         "BO":"1489760664786",
+         "LU":"1489760664786",
+         "NM":"Name",
+         "DESC":"Description",
+         "CONT":{  
+            "FNM":"John",
+            "LNM":"Doe",
+            "PHN":"5556667777",
+            "PHNTP":"NONE",
+            "EML":"ex@mple.com",
+            "EMLTP":"NONE"
+         },
+         "ADDRM":{  
+            "STRT":"1 Miracle Way",
+            "CITY":"San Antonio",
+            "STATE":"Texas",
+            "ZIP":"78245"
+         },
+         "ADDRB":{  
+            "STRT":"1 Miracle Way",
+            "CITY":"San Antonio",
+            "STATE":"Texas",
+            "ZIP":"78245"
+         },
+         "TZ":"US_CT",
+         "LICS":"1",
+         "REPOS":[  
+            {  
+               "ID":"2",
+               "TJT":"ID"
+            },
+            {  
+            }
+         ],
+         "USRS":[  
+            {  
+               "ID":"2",
+               "TJT":"ID"
+            },
+            {  
+            }
+         ]
+      }
+   }
+},
+    getDocumentType: {  
+   "ERR":0,
+   "RSP":{  
+      "DOCSTP":[  
+         {  
+            "ID":"2",
+            "TJT":"FULL",
+            "ACTV":"true",
+            "BO":"1489760664786",
+            "LU":"1489760664786",
+            "NM":"DocType Name",
+            "FLD0":{  
+               "NM":"Field Name",
+               "RQD":"true",
+               "SLNS":{  
+                  "ID":"2",
+                  "TJT":"FULL",
+                  "ACTV":"true",
+                  "BO":"1489760664786",
+                  "LU":"1489760664786",
+                  "VAL[0-29]":"Values",
+                  "DFLT":"0"
+               }
+            },
+            "FLD1":{  
+               "NM":"Field Name",
+               "RQD":"true",
+               "SLNS":{  
+                  "ID":"2",
+                  "TJT":"FULL",
+                  "ACTV":"true",
+                  "BO":"1489760664786",
+                  "LU":"1489760664786",
+                  "VAL[0-29]":"Values",
+                  "DFLT":"0"
+               }
+            },
+            "FLD2":{  
+               "NM":"Field Name",
+               "RQD":"true",
+               "SLNS":{  
+                  "ID":"2",
+                  "TJT":"FULL",
+                  "ACTV":"true",
+                  "BO":"1489760664786",
+                  "LU":"1489760664786",
+                  "VAL[0-29]":"Values",
+                  "DFLT":"0"
+               }
+            },
+            "FLD3":{  
+               "NM":"Field Name",
+               "RQD":"true",
+               "SLNS":{  
+                  "ID":"2",
+                  "TJT":"FULL",
+                  "ACTV":"true",
+                  "BO":"1489760664786",
+                  "LU":"1489760664786",
+                  "VAL[0-29]":"Values",
+                  "DFLT":"0"
+               }
+            },
+            "FLD4":{  
+               "NM":"Field Name",
+               "RQD":"true",
+               "SLNS":{  
+                  "ID":"2",
+                  "TJT":"FULL",
+                  "ACTV":"true",
+                  "BO":"1489760664786",
+                  "LU":"1489760664786",
+                  "VAL[0-29]":"Values",
+                  "DFLT":"0"
+               }
+            },
+            "FLD5":{  
+               "NM":"Field Name",
+               "RQD":"true",
+               "FMT":"DOUBLE"
+            },
+            "FLD6":{  
+               "NM":"Field Name",
+               "RQD":"true",
+               "FMT":"DOUBLE"
+            },
+            "FLD7":{  
+               "NM":"Field Name",
+               "RQD":"true",
+               "FMT":"DOUBLE"
+            },
+            "FLD8":{  
+               "NM":"Field Name",
+               "RQD":"true"
+            },
+            "FLD9":{  
+               "NM":"Field Name",
+               "RQD":"true"
+            },
+            "AUDIT":"true",
+            "ENCD":"true",
+            "CMP":"true",
+            "ACCT":{  
+               "ID":"2",
+               "TJT":"ID"
+            }
+         },
+         {  
+         }
+      ]
+   }
+},
+    getFolderandDocumentType: {  
+   "ERR":0,
+   "RSP":{  
+      "FLDR":{  
+         "ID":"2",
+         "TJT":"FULL",
+         "ACTV":"true",
+         "BO":"1489760664786",
+         "LU":"1489760664786",
+         "NM":"Name",
+         "DESC":"Description",
+         "ACCT":{  
+            "ID":"2",
+            "TJT":"ID"
+         },
+         "REPO":{  
+            "ID":"2",
+            "TJT":"ID"
+         },
+         "USR":{  
+            "ID":"2",
+            "TJT":"ID"
+         },
+         "FLDR":{  
+            "ID":"2",
+            "TJT":"ID"
+         },
+         "FLDRS":[  
+            {  
+               "ID":"2",
+               "TJT":"FULL",
+            },
+            {  
+            }
+         ],
+         "DOCS":[  
+            {  
+               "ID":"2",
+               "TJT":"FULL",
+            },
+            {  
+            }
+         ],
+         "PROPS":[  
+            {  
+               "ID":"2",
+               "TJT":"FULL",
+               "ACTV":"true",
+               "BO":"1489760664786",
+               "LU":"1489760664786",
+               "NM":"Example Name",
+               "VAL":"Example Value",
+               "PROPTP":{  
+                  "ID":"2",
+                  "TJT":"ID"
+               }
+            },
+            {  
+            }
+         ]
+      }
+   }
+}
+    
+    
+};
