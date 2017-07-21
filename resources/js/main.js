441645f70c8e882b864558ea1c6361a05f704589
Alexander Wilson van Deurs <alexwvd@outlook.com>
2017-07-21 12:23:45 +0200

    Started get doc type.

--- resources/js/main.js
+++ resources/js/main.js
@@ -26,11 +26,20 @@ function validate() {
         alert("Incorrect Username or Password.");
         count--;
     }
-    else {
+    else { /* Just for fun */
         alert("You reached the maximum amount of login attempts. You are now blocked.");
         document.login.username.value = "Blocked";
         document.login.password.value = "Blocked";
         document.login.username.disabled = true;
         document.login.password.disabled = true;
+    }
+}
+
+function getDocumentType() {
+    if (1 + 1 == 2){
+        let text = responseData;
+        for(var docs in text.getDocumentType) {
+            console.log(responseData.getDocumentType.RSP.DOCSTP["0"].NM);
+        }
     }
 }
